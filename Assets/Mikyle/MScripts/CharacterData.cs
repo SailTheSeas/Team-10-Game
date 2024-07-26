4a59f1670676d0fc32d67f5fb055535af8129fe3
MikyleFourie <mikyle.fourie@gmail.com>
2024-07-26 04:06:37 +0200

    Bare Bones StateMachine Working

---
+++ Assets/Mikyle/MScripts/CharacterData.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableObjects/CharacterData", order = 1)]
+public class CharacterData : ScriptableObject
+{
+    public string characterName;
+    public int health;
+    public int mp;
+    public int physicalAttack;
+    public int bulletAmount;
+    public int bulletDamage;
+    public List<PersonaMove> personaMoves;
+    public List<Item> items;
+}
+
---
+++ Assets/Mikyle/MScripts/EnemyCharacter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyCharacter : MonoBehaviour
+{
+    public EnemyData enemyData;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

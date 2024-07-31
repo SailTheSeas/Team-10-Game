e08851681068defc1b8f6d58fdebcc2e3cf4ef32
MikyleFourie <mikyle.fourie@gmail.com>
2024-07-31 19:30:30 +0200

    Health Bars working to reflect Player HP and MP vals and Enemy HP values

---
+++ Assets/Mikyle/MScripts/UpdateEnemyBars.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateEnemyBars : MonoBehaviour
+{
+    [SerializeField] EnemyCharacter enemyCharacter;
+    [SerializeField] Slider hpSlider;
+
+
+    void Start()
+    {
+        enemyCharacter = GetComponent<EnemyCharacter>();
+        hpSlider = this.transform.GetChild(1).GetChild(0).GetChild(1).GetComponent<Slider>();
+
+        hpSlider.maxValue = enemyCharacter.enemyHealth;
+        hpSlider.value = hpSlider.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpSlider.value = enemyCharacter.enemyHealth;
+    }
+}
---
+++ Assets/Mikyle/MScripts/UpdatePlayerBars.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdatePlayerBars : MonoBehaviour
+{
+    //[SerializeField] Canvas playerUICanvas;
+    [SerializeField] PlayerCharacter playerCharacerScript;
+    [SerializeField] Slider hpSlider;
+    [SerializeField] Slider mpSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCharacerScript = GetComponent<PlayerCharacter>();
+        hpSlider = this.transform.GetChild(1).GetChild(0).GetChild(2).GetComponent<Slider>();
+        mpSlider = this.transform.GetChild(1).GetChild(0).GetChild(3).GetComponent<Slider>();
+
+        hpSlider.maxValue = playerCharacerScript.playerHealth;
+        hpSlider.value = hpSlider.maxValue;
+        mpSlider.maxValue = playerCharacerScript.playerMP;
+        mpSlider.value = mpSlider.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpSlider.value = playerCharacerScript.PlayerHealth;
+        mpSlider.value = playerCharacerScript.playerMP;
+    }
+}
